8f83fb53f5c068f328df391b22f179f3e56d6961
Erdem C <ciceke@itu.edu.tr>
2022-11-27 00:36:15 +0300

    setup: ContactService.Api.IntegrationTests project

---
+++ ContactService.Api.IntegrationTests/TestBase.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace ContactService.Api.IntegrationTests;
+
+using static Testing;
+
+public class TestBase
+{
+    [SetUp]
+    public async Task TestSetUp()
+    {
+        //await ResetState();
+    } 
+}
