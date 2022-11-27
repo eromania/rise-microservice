3918730ffdaf0b82c575b9b0bb7f1075e0fc7c00
Erdem C <ciceke@itu.edu.tr>
2022-11-27 16:57:07 +0300

    setup: contactservice.worker console app for pub/sub

---
+++ ContactService.Worker/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+using ContactService.Worker;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+await ProcessRepositoriesAsync(client);
+
+//subscribe for the report requested event
+var queue = "report-request-quee";
+var factory = new ConnectionFactory { HostName = "localhost" }; 
+var connection = factory.CreateConnection(); 
+using var channel = connection.CreateModel();
+channel.QueueDeclare(queue);
+
+var consumer = new EventingBasicConsumer(channel); 
+consumer.Received += async (model, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    
+    await ProcessRepositoriesAsync(client);
+    
+    Console.WriteLine(message);
+};
+
+channel.BasicConsume(queue:queue , autoAck: true, consumer: consumer);
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
+{
+    //get the json data from the contact service report api
+    var json = await client.GetStringAsync("http://localhost:5200/api/report/data-by-location");
+
+    //send reportresponse message to the queue
+    RabbitMQProducer.SendMessage(json);
+    
+}
+
+
+Console.ReadKey();
+
---
+++ ContactService.Worker/RabbitMQProducer.cs
@@ -0,0 +1,21 @@
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace ContactService.Worker;
+
+public static class RabbitMQProducer
+{
+    public static void SendMessage<T>(T message)
+    {
+        var queue = "report-response-quee";
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.QueueDeclare(queue);
+
+        var json = JsonConvert.SerializeObject(message);
+        var body = Encoding.UTF8.GetBytes(json);
+        channel.BasicPublish(exchange: "", routingKey: queue, body: body);
+    }
+}
